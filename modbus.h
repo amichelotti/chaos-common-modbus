a4b831b1ac1848b02b2b95ac42e66e6d1c14ed4c
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2015-01-09 11:17:03 +0100

    fix for namespace change

--- modbus.h
+++ modbus.h
@@ -18,42 +18,42 @@
 
 namespace common {
     namespace modbus{
-      
-        
+
+
         template<class MODBUSWRAPPER>
         class ModBusTcpT:public MODBUSWRAPPER {
         public:
             ModBusTcpT(const char* _ip,int _port):MODBUSWRAPPER(_ip,_port){}
-            
+
         };
-        
+
         template<class MODBUSWRAPPER>
         class ModBusRTUT:public MODBUSWRAPPER {
         public:
             ModBusRTUT(const char* serialdev,int baudrate,char parity,int bits,int stop):MODBUSWRAPPER(serialdev,baudrate,parity,bits,stop){}
-            
+
         };
-        
+
         template<class MODBUSWRAPPER>
-        class ModBus:public MODBUSWRAPPER, public chaos::SingletonOnArguments<ModBus<MODBUSWRAPPER> >{
-            
+        class ModBus:public MODBUSWRAPPER, public chaos::common::utility::SingletonOnArguments<ModBus<MODBUSWRAPPER> >{
+
         protected:
             int callInit(std::string initialisation){return static_cast<MODBUSWRAPPER*>(this)->init(initialisation);}
 
         public:
             ModBus(){};
-           
+
             ~ModBus(){
-            
+
             }
-            
+
         };
-        
+
         typedef ModBusTcpT< ::common::modbus::LibModBusWrap> ModBusTcp;
         typedef ModBusRTUT< ::common::modbus::LibModBusWrap> ModBusRTU;
         typedef ModBus< ::common::modbus::LibModBusWrap> ModBusDrv;
 
-        
+
     }
 }
 #endif /* defined(__CHAOS_SWPowerSupplyModbusDD__ModBusTcp__) */
