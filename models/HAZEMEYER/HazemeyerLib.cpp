cd78bbe8c350dc430f1377d91c2ddc4b55f5fef0
aduffizi <alessandro.duffizi@lnf.infn.it>
2015-11-10 15:29:03 +0100

    cc

---
+++ models/HAZEMEYER/HazemeyerLib.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//#include <common/modbus/ModBus.h>
+//#include <../../common/modbus/ModBus.h>
+#include <../../common/modbus/core/LibModBusWrap.h>
+
+
+int Test()
+{
+  //common::modbus::AbstractModbus* modbus_drv=NULL;
+
+  printf("Ciao questo è un test\n");
+  return 0;
+}
+
+
+
